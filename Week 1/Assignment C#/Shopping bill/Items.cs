5ec5fb597321d3bbb0da2b92c11c5abdf2b51e64
RenovenFdo <55695651+RenovenFdo@users.noreply.github.com>
2019-10-17 15:10:09 +0530

    ASsignment C#

---
+++ Week 1/Assignment C#/Shopping bill/Items.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping_bill
+{
+    class Items
+    {
+        string[] food;
+        int[] quantity;
+        enum price { idli = 5, dosai = 10, chappathi = 15, poori = 6, pongal = 20 };
+        public Items()
+        {
+
+            int num;
+            Console.WriteLine("Enter number of items you wish to buy");
+            num = int.Parse(Console.ReadLine());
+            food = new string[num];
+            quantity = new int[num];
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("Enter the item you wish to buy and quantity");
+                food[i] = Console.ReadLine().ToLower();
+                quantity[i] = int.Parse(Console.ReadLine());              
+            }
+            Console.WriteLine("GENERATED BILL");
+            Console.WriteLine("ITEM\tQUANTITY    PRICE");
+            int sum = 0;
+            for (int i = 0; i < num; i++)
+            {
+                int rate = quantity[i] * (int)Enum.Parse(typeof(price), food[i]);
+                sum = sum + rate;
+                Console.WriteLine();
+                Console.Write("{0}\t  {1}\t    {2}",food[i].ToUpper(), quantity[i], rate);
+            }
+                Console.WriteLine("\nAMOUNT TO BE PAID=  "+sum);
+
+        }
+
+    }
+}
+
+
+
+
---
+++ Week 1/Assignment C#/Shopping bill/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shopping_bill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\tItems Available");
+            Console.WriteLine("\tIdli\n\tDosai\n\tChappathi\n\tPoori\n\tPongal");
+            Items item = new Items();
+            Console.ReadLine();
+        }
+    }
+}
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Product
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int num;
+//            Console.WriteLine("ProductList     Cost per Kg");
+//            Console.WriteLine("-----------     -----------\n");
+//            Console.WriteLine("1.Apple \t- Rs 50\n2.Mango \t- Rs 60\n3.Banana \t- Rs 30\n4.Orange \t- Rs 45\n5.Pomegranate \t- Rs 60\n");
+//            Console.WriteLine("Enter the no.of products you want to purchase");
+//            num = int.Parse(Console.ReadLine());
+//            Product product = new Product(num);
+//            Console.WriteLine("Enter the product with its quantity one by one");
+//            for (int i = 0; i < num; i++)
+//            {
+//                string name = "";
+//                bool set = true;
+//                while (set)
+//                {
+//                    name = Console.ReadLine();
+//                    if (name.ToLower() != "apple" && name.ToLower() != "mango" && name.ToLower() != "banana" && name.ToLower() != "orange" && name.ToLower() != "pomegranate")
+//                        Console.WriteLine("Enter a valid product");
+//                    else
+//                        set = false;
+//                }
+//                int qty = int.Parse(Console.ReadLine());
+//                product.cart(i, char.ToUpper(name[0]) + name.Substring(1).ToLower(), qty);
+//            }
+//            product.bill();
+//            Console.ReadKey();
+//        }
+//    }
+//}
